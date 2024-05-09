4a9eb1ba7c65501ee878730ab972a93ead27e62a
wanjiku <annritawanjiku77@gmail.com>
2024-05-09 08:51:54 +0300

    connect login and registerforms

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Form from '../components/Form'
 
 function Login() {
   return (
-    <div>Login</div>
+    <Form route="/api/token/" method="login" />
+
   )
 }
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Form from '../components/Form'
+
 
 function Register() {
   return (
-    <div>Register</div>
+    <Form route="/api/user/register/" method="register" />
   )
 }
 
