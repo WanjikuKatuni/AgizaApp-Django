013fee797e62ccc6a73c4ad91603f18bbc0dbd59
wanjiku <annritawanjiku77@gmail.com>
2024-05-10 16:07:02 +0300

    handle migrations

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Customer(models.Model):
     customerName = models.CharField(max_length=50)
     customerCode = models.CharField(max_length=50)
-    phonenumber = models.CharField(max_length=15)
+    phonenumber = models.CharField(max_length=50)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
