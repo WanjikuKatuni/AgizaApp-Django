feea27cd1a42ce8be9f85844536440be96155614
wanjiku <annritawanjiku77@gmail.com>
2024-05-16 13:22:54 +0300

    deploy

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.conf import settings
+from django.views.static import serve
 
 # User registration and token authentication endpoints
 urlpatterns = [
+
+    
     # auth/registration urls
     path('admin/', admin.site.urls),
     path("api/user/register/", CreateUserView.as_view(), name="register"),
@@ -12,6 +16,10 @@ urlpatterns = [
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
     # api urls
-    path("api/", include("api.urls"))
+    path("api/", include("api.urls")),
+
+
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}), 
 
 ]
